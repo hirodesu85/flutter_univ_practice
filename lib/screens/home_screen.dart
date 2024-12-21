938cb9e01112126f76bcc1330c2639fd7ee787b5
OGATA Hiromu <hiromukkun@gmail.com>
2024-12-21 20:01:35 +0900

    delete:リセット

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-  static List<String> entries = <String>['A', 'B', 'C', 'D', 'E'];
-  static List<int> colorCodes = <int>[600, 500, 100, 200, 300];
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +13,6 @@ class HomeScreen extends StatelessWidget {
         body: Container(
             // 色を指定
             color: Colors.blue,
-            child: ListView.separated(
-                padding: const EdgeInsets.all(8),
-                itemCount: entries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 50,
-                    color: Colors.amber[colorCodes[index]],
-                    child: Center(child: Text('Entry ${entries[index]}')),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider())));
+            child: Container()));
   }
 }
