63fd6856d28ae359ac1b5e965b92d17c13a5329e
OGATA Hiromu <hiromukkun@gmail.com>
2024-12-21 19:55:39 +0900

    add:listviewを使う

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_univ_practice/screens/second_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  static List<String> entries = <String>['A', 'B', 'C', 'D', 'E'];
+  static List<int> colorCodes = <int>[600, 500, 100, 200, 300];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('ホーム'),
-      ),
-      body: Container(
-        // 色を指定
-        color: Colors.blue,
-        child: Center(
-          child: Image.asset('images/icon.JPG'),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('ホーム'),
         ),
-      ),
-    );
+        body: Container(
+            // 色を指定
+            color: Colors.blue,
+            child: ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: entries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    color: Colors.amber[colorCodes[index]],
+                    child: Center(child: Text('Entry ${entries[index]}')),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider())));
   }
 }
