d649e2bf8660948ff0ab4b6e3d8f4a70a27b3b31
OGATA Hiromu <hiromukkun@gmail.com>
2024-12-22 17:38:57 +0900

    add:firestoreから値を取得

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,262 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _fetchFirebaseData() async {
+    await FirebaseFirestore.instance.collection("posts").get().then((event) {
+      for (var doc in event.docs) {
+        print("${doc.id} => ${doc.data()}");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('Youtube Trace'),
+      appBar: AppBar(
+        title: const Text("FireStore Practice"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Firestoreのデータを取得する",
+            ),
+            Text(
+              'データを取得するボタンを押してください',
+            ),
+          ],
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView(
-              children: <Widget>[
-                SizedBox(
-                  height: 100,
-                  width: double.infinity,
-                  child: Image.network(
-                      'https://momochy.com/wp-content/uploads/2023/01/%E3%83%98%E3%83%83%E3%83%80%E3%83%BC_02.jpg',
-                      fit: BoxFit.cover),
-                ),
-                const Row(
-                  children: [
-                    Icon(
-                      Icons.person,
-                      size: 100,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('User Name',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                        Text('@user_name',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                        Text('21.5K followers',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.person_add),
-                            Text('Follow'),
-                          ],
-                        ))),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 140,
-                      height: 70,
-                      child: Image.network(
-                        'https://medibangpaint.com/wp-content/uploads/2022/10/4c266d3ba843fe327b3c6ebf237a6678.jpg',
-                      ),
-                    ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('ソフトウェア工学の期末試験は難しい',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text('244 vies・1 day ago',
-                              style: TextStyle(
-                                fontSize: 14,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _fetchFirebaseData,
+        tooltip: 'Fetch Firebase Data',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
